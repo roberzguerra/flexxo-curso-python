b84ee5382028c4e7eccbd928698ef477ec165a63
Rober Guerra <roberzguerra@gmail.com>
2018-10-15 22:20:03 -0300

    Primeiro commit da aula

---
+++ python.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Template padrão para scripts python
+
+__author__="Rober Guerra"
+__date__ ="$15/10/2018 21:00:00$"
+
+
+def dolar(valor):
+    print("Converter em Dolar")
+    return valor * 3.74
+
+
+if __name__ == "__main__":
+    resultado = dolar(100)
+    print("Resultado = %f" % resultado)
+    
+
+
+
+    
